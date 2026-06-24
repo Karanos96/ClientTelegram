daf710ac3c737acbcec0ac2baa2e6b3beec93003
R3d_kn1ght <a.giuri96@gmail.com>
2026-06-25 00:48:52 +0200

    Add key generator

---
+++ KeyGenerator/KeyGenerator/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Execute this for the first time, after save in the secret.
+byte[] key = System.Security.Cryptography.RandomNumberGenerator.GetBytes(32);
+string base64Key = Convert.ToBase64String(key);
+Console.WriteLine(base64Key); // copy this and put in the secret / system environment 
